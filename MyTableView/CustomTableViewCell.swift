d880dba5cb71a0f1309faee13e8d93205cc49544
ArturDokhno <artur.dohno@icloud.com>
2021-11-19 18:17:51 +0500

    Создали кастовую ячейку для отображения данных в ней

---
+++ MyTableView/CustomTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTableViewCell.swift
+//  MyTableView
+//
+//  Created by Артур Дохно on 19.11.2021.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet var imageOfPlace: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var lacationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    
+}
