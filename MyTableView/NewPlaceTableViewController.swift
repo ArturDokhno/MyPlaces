25099baddc0c79cd9b745321e8507f5623256368
ArturDokhno <artur.dohno@icloud.com>
2021-11-19 20:28:59 +0500

    Работа со сторибор NewPlaceTableViewController

---
+++ MyTableView/NewPlaceTableViewController.swift
@@ -0,0 +1,41 @@
+//
+//  NewPlaceTableViewController.swift
+//  MyTableView
+//
+//  Created by Артур Дохно on 19.11.2021.
+//
+
+import UIKit
+
+class NewPlaceTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.tableFooterView = UIView()
+    }
+    
+    // MARK: - Table view delegate
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == 0 {
+            
+        } else {
+            view.endEditing(true)
+        }
+    }
+
+}
+
+// MARK: - Text field delegate
+
+extension NewPlaceTableViewController: UITextFieldDelegate {
+    
+    // скрываем клавиатуру по нажатию на Done
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+}
