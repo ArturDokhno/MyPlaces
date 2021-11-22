accb04e23ab3861d1c8fcca028cd958555ee4af6
ArturDokhno <artur.dohno@icloud.com>
2021-11-22 20:37:47 +0500

    Реализация метода удаление ячейки из таблицы и базы данных соответственно.

--- MyTableView/StorageManager.swift
+++ MyTableView/StorageManager.swift
@@ -13,8 +13,15 @@ class StorageManager {
     
     static func saveObject(_ place: Place) {
         
-        try! realm.write({
+        try! realm.write{
             realm.add(place)
-        })
+        }
+    }
+    
+    static func deleteObject(_ place: Place) {
+        
+        try! realm.write{
+            realm.delete(place)
+        }
     }
 }
