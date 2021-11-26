0908c1c7c05f68f221dbc38b8a701272bd53d7e5
ArturDokhno <artur.dohno@icloud.com>
2021-11-26 21:21:03 +0500

    initial commit

---
+++ MyTableView/MainViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MainViewController.swift
+//  MyTableView
+//
+//  Created by Артур Дохно on 26.11.2021.
+//
+
+import UIKit
+
+class MainViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        return cell
+    }
+
+    // MARK: - Navigation
+
+}
