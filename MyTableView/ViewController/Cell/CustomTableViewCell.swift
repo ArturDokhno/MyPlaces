58bcca8b3b5eb51435cd66a2bcc9de24756249f6
ArturDokhno <artur.dohno@icloud.com>
2021-11-27 01:36:54 +0500

    Добавление CustomTableViewCell

---
+++ MyTableView/ViewController/Cell/CustomTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTableViewCell.swift
+//  MyTableView
+//
+//  Created by Артур Дохно on 27.11.2021.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet var imageOfPlace: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationlabel: NSLayoutConstraint!
+    @IBOutlet var typeLabel: UILabel!
+    
+}
