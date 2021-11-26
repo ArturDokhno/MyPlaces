7b5511826a11e65f2182f6c9f4aa9b323a614563
ArturDokhno <artur.dohno@icloud.com>
2021-11-26 21:49:58 +0500

    Добавление массива названий ресторанов и отображение их в таблице

--- MyTableView/MainViewController.swift
+++ MyTableView/MainViewController.swift
@@ -8,6 +8,13 @@
 import UIKit
 
 class MainViewController: UITableViewController {
+    
+    let restaurantNames = [
+        "Traveler's Coffee", "Дрова", "Кофеин", "Joint Pub",
+        "MISHKA BAR", "Мао", "Cafe Botanica", "Traveler's Coffee",
+        "Болгарская роза", "Семейная пиццерия", "People's",
+        "Диван-Сарай", "Hurma", "На высоте", "Мерцана"
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,15 +24,21 @@ class MainViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return restaurantNames.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        var conten = cell.defaultContentConfiguration()
+        
+        conten.text = restaurantNames[indexPath.row]
 
+        cell.contentConfiguration = conten
         return cell
     }
 
     // MARK: - Navigation
 
+    
+    
 }
