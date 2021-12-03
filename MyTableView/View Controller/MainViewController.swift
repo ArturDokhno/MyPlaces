de59903df7ac13b11c0ab80cb580b133e4ac6c52
ArturDokhno <artur.dohno@icloud.com>
2021-12-03 13:34:15 +0500

    Реализация поиска места по названию и локации. Переделали отображения ячеек в зависимости от того пустая строка поиска или нет, создав для этого массив отфильтрованных мест.

--- MyTableView/View Controller/MainViewController.swift
+++ MyTableView/View Controller/MainViewController.swift
@@ -10,20 +10,56 @@ import RealmSwift
 
 class MainViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    @IBOutlet var tableView: UITableView!
-    @IBOutlet var segmentedControl: UISegmentedControl!
-    @IBOutlet var reversedSortingButton: UIBarButtonItem!
+    // обьявляем экземпляр класса UISearchController
+    // searchResultsController передаем nil
+    // что бы сообщить UISearchController что будем использовать тот же
+    // View для отображения результатов поиска
     
+    private var searchController = UISearchController(searchResultsController: nil)
     
     // загружаем наши данные из таблицы Realm
     // Results позволяет работать с данными в реальном времени
     
-    var places: Results<Place>!
+    private var places: Results<Place>!
     
     // вспомогательно свойство для сортировки возростанию или убыванию
     // которое мы будем менять при нажатии кнопки
     
-    var ascendingSorting = true
+    private var ascendingSorting = true
+    
+    // массив в котором будет хранится отфильтрованые данные
+    // с помощью поисковика
+    
+    private var filteredPlaces: Results<Place>!
+    
+    // булевое значение которое указывает пустая строка поиска или нет
+    
+    private var seachBarIsEmpty: Bool {
+        
+        // извлекаем опционал текст из поиска если не получится
+        // вернем фолс, значит строка поиска не пустая
+        
+        guard let text = searchController.searchBar.text else { return false }
+        
+        // если строка поиска пустая вернется тру
+        
+        return text.isEmpty
+    }
+    
+    // свойсвто которое будет отслеживать что отображать в таблице
+    // данные из массива filteredPlaces или places
+    
+    private var isFiltering: Bool {
+        
+        // поисковоя строка активирована и не пустая
+        // тогда вернем тру
+        
+        return searchController.isActive && !seachBarIsEmpty
+    }
+    
+    @IBOutlet var tableView: UITableView!
+    @IBOutlet var segmentedControl: UISegmentedControl!
+    @IBOutlet var reversedSortingButton: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,23 +71,72 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
         
         places = realm.objects(Place.self)
         
+        // Настраиваем ascendingSorting
+        
+        // сообщаем segmentedControl что получаетелем изменений текста
+        // в поисковите должен быть данный класс
+        
+        searchController.searchResultsUpdater = self
+        
+        // позволяем управлять данными который выдаст поиск
+        // сможем редактировать или удалять ячейки
+        
+        searchController.obscuresBackgroundDuringPresentation = false
+        
+        // присваиваем название поисковой строки
+        
+        searchController.searchBar.placeholder = "Search"
+        
+        // интегрируем строку поиска в navigation bar
+        
+        navigationItem.searchController = searchController
+        
+        // позвоялем отпускать строку поиска при переходе на другой экран
+        
+        definesPresentationContext = true
+        
     }
     
     // MARK: - Table view data source
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
+        // если isFiltering возвращает тру то
+        // отображаем количесво ячеек из массива filteredPlaces
+        // иначе из массива places
+        
+        if isFiltering {
+            return filteredPlaces.count
+        }
+    
         // если база пустая возвращаем количесво ячеек 0
         // если нет то возвращаем количесво ячеек равной
         // количеству моделей в базе
         
         return places.isEmpty ? 0 : places.count
+        
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
         
-        let place = places[indexPath.row]
+        var place = Place()
+        
+        // если isFiltering тру то присваиваем массиву place
+        // значения из массива filteredPlaces по индексу
+        
+        if isFiltering {
+            
+            place = filteredPlaces[indexPath.row]
+            
+        } else {
+            
+            // если fals то присваиваем массиву place
+            // значения из массива places по индексу
+            
+            place = places[indexPath.row]
+            
+        }
         
         cell.nameLabel.text = place.name
         cell.locationlabel.text = place.location
@@ -117,9 +202,21 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
             
             guard let indexPath = tableView.indexPathForSelectedRow else { return }
             
-            // извлекаем обьект из массива places по текущему индексу
+            // повторяем тот же если иначе что бы
+            // выбиралась ячейка при фитрации кореткно
+            // в виду того что у нас два разных массива
+            // а ячейка для редактирования выбирается из основного массива
+            // при фильтрации мы выбирали не коретную ячейку так как индексы ячеек
+            // не совпадали для этого мы сделали выбор ячейки в зависимости
+            // если текст в поисковике или нет 
             
-            let place = places[indexPath.row]
+            let place: Place
+            
+            if isFiltering {
+                place = filteredPlaces[indexPath.row]
+            } else {
+                place = places[indexPath.row]
+            }
             
             // передаем данные с ячейки которую выбрали в NewPlaceViewController
             // с помощью атрибута destination у segue
@@ -217,5 +314,38 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
         tableView.reloadData()
     }
     
+}
+
+extension MainViewController: UISearchResultsUpdating {
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        
+        // вызываем метод фильтрации поставляя туда
+        // подставляем в параметр значение из поиской строки
+        // можем извлеч опцианал так как метод updateSearchResults будет вызываться
+        // только когда будем нажимать на поисковую строку
+        // даже если строка будет пустой она не будут нил
+        
+        filterContentForSearchText(searchController.searchBar.text!)
+    }
+    
+    // метод фильтрации контента
+    
+    private func filterContentForSearchText(_ searchText: String) {
+        
+        // заполняем массив отфильтроваными результатами
+        // выполняем поиск по полям name и location
+        // поиск совершаем по CONTAINS
+        // в независимости от регистра символов указав [c]
+        // в %@ подставляются переменная searchText
+        
+        filteredPlaces = places.filter("name CONTAINS[c] %@ OR location CONTAINS[c] %@",
+        searchText, searchText)
+        
+        // обновляем таблицу
+        
+        tableView.reloadData()
+        
+    }
     
 }
